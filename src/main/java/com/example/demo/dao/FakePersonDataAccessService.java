05985272d9d7ea0f17b92295cfa1ecdf47d73a92
ndowarp <nicolas961945@gmail.com>
2021-05-07 16:22:55 -0500

    Added get all end point
    Added get all end point to retrieve all people

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository("fakeDao")
-public class FakePersonDataAccessService implements PersonDao{
+public class FakePersonDataAccessService implements PersonDao {
 
     private static List<Person> DB = new ArrayList<>();
 
@@ -16,5 +16,10 @@ public class FakePersonDataAccessService implements PersonDao{
     public int insertPerson(UUID id, Person person) {
         DB.add(new Person(id,person.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
     }
 }
